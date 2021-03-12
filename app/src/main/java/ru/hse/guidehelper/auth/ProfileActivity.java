97ecb36e9599ebfc7b4297228eca4f1c3a6ce8a4
smirok <smirok123@mail.ru>
2021-03-13 01:12:45 +0300

    init google auth

---
+++ app/src/main/java/ru/hse/guidehelper/auth/ProfileActivity.java
@@ -0,0 +1,16 @@
+package ru.hse.guidehelper.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.hse.guidehelper.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
