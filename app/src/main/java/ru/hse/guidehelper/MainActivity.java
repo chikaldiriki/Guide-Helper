811f723ba487683d2ac14244368b2f1c4114903f
Usoltsev Nikita <usoltsev-nv@yandex.ru>
2021-05-21 02:13:44 +0300

    deleted unused imports 3

--- app/src/main/java/ru/hse/guidehelper/MainActivity.java
+++ app/src/main/java/ru/hse/guidehelper/MainActivity.java
@@ -25,7 +25,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.nio.file.Files;
 import java.util.Objects;
 
 import ru.hse.guidehelper.config.ApplicationConfig;
