bbba41c82c66637634052a4c942740bdecf04d22
Usoltsev Nikita <usoltsev-nv@yandex.ru>
2021-05-26 01:01:43 +0300

    fixed link

--- app/src/main/java/ru/hse/guidehelper/utils/ClientUtils.java
+++ app/src/main/java/ru/hse/guidehelper/utils/ClientUtils.java
@@ -4,7 +4,7 @@ import okhttp3.OkHttpClient;
 
 public class ClientUtils {
     public static final String url = "http://192.168.0.62:8080";
-    public static final String suffTours = "/tours";
+    public static final String suffTours = "/tours/all";
     public static final String suffUsers = "/users";
     public static final String suffMessages = "/messages";
     public static final String suffChat = "/chat";
