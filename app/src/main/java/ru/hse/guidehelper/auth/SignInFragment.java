e84408ce29832fe7bb79fc6ef3a53625c1385001
Nikita Usoltsev <57728940+usoltsev37@users.noreply.github.com>
2021-05-31 19:30:33 +0300

    fixed codestyle 2

--- app/src/main/java/ru/hse/guidehelper/auth/SignInFragment.java
+++ app/src/main/java/ru/hse/guidehelper/auth/SignInFragment.java
@@ -23,7 +23,6 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 import ru.hse.guidehelper.MainActivity;
