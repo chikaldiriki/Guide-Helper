d22137739a8cebb914e03582f0929cc1a3ed9974
smirok <smirok123@mail.ru>
2021-06-07 02:34:50 +0300

    Made tour list UI more nicely

--- app/src/main/java/ru/hse/guidehelper/excursions/TourRecyclerViewAdapter.java
+++ app/src/main/java/ru/hse/guidehelper/excursions/TourRecyclerViewAdapter.java
@@ -1,14 +1,22 @@
 package ru.hse.guidehelper.excursions;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
@@ -52,10 +60,21 @@ public abstract class TourRecyclerViewAdapter
         return new TourRecyclerViewAdapter.ViewHolder(view);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(final TourRecyclerViewAdapter.ViewHolder holder, int position) {
-        holder.title.setText(tours.get(position).getTitle());
-        holder.index.setText(String.valueOf(position + 1));
+        holder.excursionName.setText(tours.get(position).getTitle());
+
+        if (tours.get(position).getImage() != null) {
+            byte[] imageByteArray = Base64.getDecoder().decode(tours.get(position).getImage());
+            Bitmap image = BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.length);
+            holder.imageImageView.setImageBitmap(image);
+        } else {
+            holder.imageImageView.setImageResource(R.drawable.ic_launcher_background);
+        }
+        String cost = tours.get(position).getCost().toString() + ' ' + Html.fromHtml(" &#x20bd");
+
+        holder.costTextView.setText(cost);
         holder.itemView.setTag(tours.get(position));
         holder.itemView.setOnClickListener(mOnClickListener);
     }
@@ -66,13 +85,15 @@ public abstract class TourRecyclerViewAdapter
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        public final TextView index;
-        public final TextView title;
+        public final TextView excursionName;
+        public final TextView costTextView;
+        public final ImageView imageImageView;
 
         ViewHolder(View view) {
             super(view);
-            index = view.findViewById(R.id.id_text);
-            title = view.findViewById(R.id.content);
+            excursionName = view.findViewById(R.id.id_excursionNameTextView);
+            costTextView = view.findViewById(R.id.excursionsCostTextView);
+            imageImageView = view.findViewById(R.id.excursionsImageImageView);
         }
     }
 }
