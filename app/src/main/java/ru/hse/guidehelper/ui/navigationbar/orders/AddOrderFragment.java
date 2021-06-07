e666929b2f928553a9197cf485b124ac60041a29
Nikita Usoltsev <57728940+usoltsev37@users.noreply.github.com>
2021-06-08 01:29:46 +0300

    added addOrder.xml

---
+++ app/src/main/java/ru/hse/guidehelper/ui/navigationbar/orders/AddOrderFragment.java
@@ -0,0 +1,24 @@
+package ru.hse.guidehelper.ui.navigationbar.orders;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import ru.hse.guidehelper.R;
+
+public class AddOrderFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_add_order, container, false);
+        return root;
+    }
+}
